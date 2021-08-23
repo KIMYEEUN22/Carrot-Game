48757009e078740489f1f5840c2b418367003698
KIMYEEUN22 <kye9702@naver.com>
2021-08-23 14:18:26 +0900

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,37 +1,37 @@
-'use strict';
-import PopUp from './popup.js';
-import * as sound from './sound.js';
-import { GameBuilder, Reason } from './game.js';
+"use strict";
+import PopUp from "./popup.js";
+import * as sound from "./sound.js";
+import { GameBuilder, Reason } from "./game.js";
 
 const gameFinishBanner = new PopUp();
 const game = new GameBuilder()
-    .withGameDuration(5)
-    .withCarrotCount(3)
-    .withBugCount(3)
-    .build();
+  .withGameDuration(5)
+  .withCarrotCount(7)
+  .withBugCount(7)
+  .build();
 
-game.setGameStopListner((reason => {
-    console.log(reason);
-    let message;
-    switch(reason){
-        case Reason.cancel:
-            message = 'REPLAY❓';
-            sound.playAlert;
-            break;
-        case Reason.win:
-            message = 'YOU WIN🎉';
-            sound.playWin;
-            break;
-        case Reason.lost:
-            message = 'YOU LOST💩';
-            sound.playBug;
-            break;
-        default: throw new Error('not valid reason');
-
-    }
-    gameFinishBanner.showText(message);
-}))
+game.setGameStopListner((reason) => {
+  console.log(reason);
+  let message;
+  switch (reason) {
+    case Reason.cancel:
+      message = "REPLAY❓";
+      sound.playAlert;
+      break;
+    case Reason.win:
+      message = "YOU WIN🎉";
+      sound.playWin;
+      break;
+    case Reason.lost:
+      message = "YOU LOST💩";
+      sound.playBug;
+      break;
+    default:
+      throw new Error("not valid reason");
+  }
+  gameFinishBanner.showText(message);
+});
 
 gameFinishBanner.setClickListner(() => {
-    game.start();
+  game.start();
 });
